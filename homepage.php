624ae1fdebd580c6d7853d9bf253e12ac77d2a17
Gihan_Sachith <gihansachith92@gmail.com>
2023-12-26 18:18:25 +0530

    change

--- homepage.php
+++ homepage.php
@@ -5,25 +5,71 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TechHaven</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <style>
     body {
       margin: 0;
-      padding: 0;
+      
       background-size: cover;
       background-position: center;
       font-family: sans-serif;
-      background: #1e1b1b; /* Background color set to #1e1b1b */
+      background: #1e1b1b; 
+      padding-top: 56px;/* Background color set to #1e1b1b */
     }
+    .navbar {
+            background-color: transparent;
+        }
+
+        .navbar-toggler-icon {
+            background-color: orange;
+        }
+
+        .navbar-nav .nav-link {
+            color: orange !important; /* Override Bootstrap styles for link color */
+        }
+
+        .navbar-nav .nav-item.active .nav-link {
+            color: white !important; /* Override Bootstrap styles for active link color */
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: white !important; /* Override Bootstrap styles for link color on hover */
+        }
   </style>
 </head>
 
 <body>
-<?php
-    // Assuming you have a PHP file named 'head.php' for including the head content
-    include_once 'head.php';
-  ?> 
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <a class="navbar-brand" href="#">Your Brand</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
