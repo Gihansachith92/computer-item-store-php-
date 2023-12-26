8624f6f48961809add36406e82ce8e98467b345a
Gihan_Sachith <gihansachith92@gmail.com>
2023-12-26 16:49:36 +0530

    add css to homepage

--- home.php
+++ home.php
@@ -4,8 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TechHaven</title>
+  <style>
+    body{
+      margin: 0;
+      padding: 0;
+      background-size: cover;
+      background-position: center;
+      font-family: sans-serif;
+    }
+  </style>
 </head>
 <body>
-  
+  <img src="Assest/Bg.jpg" alt="bg image">
 </body>
 </html>
